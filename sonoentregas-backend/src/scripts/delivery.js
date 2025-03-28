590a71ba0749e78afb43dd6b6c02a3deeee83c90
Alexandre Silva Santos <alex_ff2@hotmail.com>
2025-03-27 22:10:25 -0300

    fix(delivery): Update SQL queries to filter by delivery status and improve readability

--- sonoentregas-backend/src/scripts/delivery.js
+++ sonoentregas-backend/src/scripts/delivery.js
@@ -1,6 +1,7 @@
 module.exports = {
   findToBeepDelivery(id) {
-    return `SELECT B.CODIGO id, B.APLICACAO mask, B.NOME [nameFull], SUM(A.QTD_DELIV) quantity,
+    return `
+      SELECT B.CODIGO id, B.APLICACAO mask, B.NOME [nameFull], SUM(A.QTD_DELIV) quantity,
       ISNULL(C.quantityBeep, 0) quantityBeep, A.ID_DELIVERY moduleId, B.SUBG subGroupId, 
       (SUM(A.QTD_DELIV) - ISNULL(C.quantityBeep, 0)) quantityPedding, 'delivery' module
       FROM ${process.env.ENTREGAS_BASE}..DELIVERYS_PROD A
@@ -14,7 +15,7 @@ module.exports = {
       GROUP BY A.ID_DELIVERY, B.CODIGO,  B.APLICACAO, B.NOME, C.quantityBeep, B.SUBG
       UNION
       SELECT C.CODIGO id, C.APLICACAO mask, C.NOME nameFull, A.QUANTIDADE quantity,
-      ISNULL(D.quantityBeep, 0) quantityBeep, A.ID moduleId, C.SUBG subGroupId,
+      ISNULL(D.quantityBeep, 0) quantityBeep, B.ID moduleId, C.SUBG subGroupId,
       A.QUANTIDADE - ISNULL(D.quantityBeep, 0) quantityBeep, 'maintenance' module
       FROM ${process.env.ENTREGAS_BASE}..MAINTENANCE A
       INNER JOIN ${process.env.ENTREGAS_BASE}..MAINTENANCE_DELIV B ON A.ID = B.ID_MAINT
@@ -23,7 +24,7 @@ module.exports = {
           FROM PRODLOJAS_SERIES_MOVIMENTOS
           WHERE outputModule = 'maintenance'
           GROUP BY productId, outputModuleId) D ON D.productId = C.CODIGO AND D.outputModuleId = A.ID
-      WHERE B.ID_DELIV_MAIN = ${id}`
+      WHERE B.DONE = 1 AND B.ID_DELIV_MAIN = ${id}`
   },
   findToBeepDeliveryReturn(id) {
     return `
@@ -40,8 +41,20 @@ module.exports = {
       WHERE A.ID_DELIVERY = ${id} AND A.DELIVERED = 1
       GROUP BY A.ID_DELIVERY, B.CODIGO,  B.APLICACAO, B.NOME, C.quantityBeep, B.SUBG
       UNION
+      SELECT C.CODIGO id, C.APLICACAO mask, C.NOME nameFull, A.QUANTIDADE quantity,
+      ISNULL(D.quantityBeep, 0) quantityBeep, B.ID moduleId, C.SUBG subGroupId,
+      A.QUANTIDADE - ISNULL(D.quantityBeep, 0) quantityBeep, 'maintenance' module
+      FROM ${process.env.ENTREGAS_BASE}..MAINTENANCE A
+      INNER JOIN ${process.env.ENTREGAS_BASE}..MAINTENANCE_DELIV B ON A.ID = B.ID_MAINT
+      INNER JOIN PRODUTOS C ON C.ALTERNATI = A.COD_ORIGINAL
+      LEFT JOIN ( SELECT productId, COUNT(id) quantityBeep, inputModuleId
+          FROM PRODLOJAS_SERIES_MOVIMENTOS
+          WHERE inputModule = 'maintenance'
+          GROUP BY productId, inputModuleId) D ON D.productId = C.CODIGO AND D.inputModuleId = A.ID
+      WHERE B.DONE = 0 AND B.ID_DELIV_MAIN = ${id}
+      UNION
       SELECT C.CODIGO id, C.APLICACAO mask, C.NOME nameFull, B.quantity,
-      ISNULL(D.quantityBeep, 0) quntityBeep, A.id moduleId, C.SUBG subGroupId,
+      ISNULL(D.quantityBeep, 0) quntityBeep, B.id moduleId, C.SUBG subGroupId,
       B.quantity - ISNULL(D.quantityBeep, 0) quantityPedding, 'saleReturn' module
       FROM ${process.env.ENTREGAS_BASE}..RETURNS_SALES A
       INNER JOIN ${process.env.ENTREGAS_BASE}..RETURNS_SALES_PRODUCTS B ON A.id = B.returnsSalesId
