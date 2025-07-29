f1955bb105eab9172090d298183c9c3db1ce4b4b
Alexandre Silva Santos <alex_ff2@hotmail.com>
2025-07-29 20:14:19 -0300

    feat(products): Add product stock search script with beep and without beep

--- sonoentregas-backend/src/scripts/products.js
+++ sonoentregas-backend/src/scripts/products.js
@@ -27,5 +27,77 @@ module.exports = {
       ) C ON C.ALTERNATI = A.COD_ORIGINAL
       GROUP BY  A.DESCRICAO, A.PEDIDO, B.FACTORY_DATA, C.EST_ATUAL
     ) A WHERE A.EST_ATUAL < A.QTD`
-  }
+  },
+  stockBeep(condition, type = 'IN'){
+    const where = Object.keys(condition) === 'COD_ORIGINAL' 
+      ? `A.ALTERNATI IN (${Object.values(condition)})`
+      : `A.${Object.keys(condition)} ${type === 'IN' ? `IN (${Object.values(condition)})` : `LIKE '${Object.values(condition)}%'`}`
+
+      return`SELECT A.CODIGO, A.ALTERNATI COD_ORIGINAL, A.NOME, ISNULL(C.EST_LOJA, 0) EST_LOJA, ISNULL(B.qtdForecast, 0) qtdForecast, ISNULL(D.qtdTransfer, 0) qtdTransfer, ISNULL(C.EST_LOJA, 0) - ISNULL(B.qtdForecast, 0) - ISNULL(D.qtdTransfer, 0) availableStock
+    FROM PRODUTOS A
+    LEFT JOIN (
+      SELECT A.COD_ORIGINAL, A.qtdForecast - ISNULL(B.qtdBeep, 0) qtdForecast FROM (
+        SELECT B.COD_ORIGINAL, SUM(B.quantityForecast) qtdForecast
+        FROM ${process.env.ENTREGAS_BASE}..FORECAST_SALES A
+        INNER JOIN ${process.env.ENTREGAS_BASE}..FORECAST_PRODUCT B ON A.id = B.idForecastSale
+        INNER JOIN ${process.env.ENTREGAS_BASE}..FORECAST C ON C.id = A.idForecast
+        WHERE (C.status IS NULL OR C.status = 1)
+        GROUP BY B.COD_ORIGINAL) A
+      LEFT JOIN (
+        SELECT B.ALTERNATI, COUNT(*) qtdBeep
+        FROM PRODLOJAS_SERIES_MOVIMENTOS A
+        INNER JOIN PRODUTOS B ON A.productId = B.CODIGO
+        WHERE A.outputBeepDate IS NOT NULL AND outputModule = 'delivery' AND A.outputModuleId IN (
+          SELECT A.idDelivery
+          FROM ${process.env.ENTREGAS_BASE}..FORECAST_SALES A
+          INNER JOIN ${process.env.ENTREGAS_BASE}..FORECAST_PRODUCT B ON A.id = B.idForecastSale
+          INNER JOIN ${process.env.ENTREGAS_BASE}..FORECAST C ON C.id = A.idForecast
+          WHERE (C.status IS NULL OR C.status = 1)
+        )
+        GROUP BY B.ALTERNATI
+      ) B ON A.COD_ORIGINAL = B.ALTERNATI
+    ) B ON A.ALTERNATI = B.COD_ORIGINAL
+    LEFT JOIN (
+      SELECT productId, COUNT(*) EST_LOJA
+        FROM PRODLOJAS_SERIES_MOVIMENTOS
+        WHERE outputBeepDate IS NULL
+        GROUP BY productId
+    ) C ON C.productId = A.CODIGO
+    LEFT JOIN (
+      SELECT A.CODPRODUTO, A.QUANTIDADE - ISNULL(B.qtd_beep, 0) qtdTransfer
+      FROM (
+        SELECT B.CODPRODUTO, SUM(B.QUANTIDADE) QUANTIDADE
+        FROM TRANSPRODLOJA A
+        INNER JOIN TRANSPRODLOJAI B ON A.CODIGO = B.REGISTRO
+        WHERE A.EMISSAO >= '2025-04-08' AND A.LOJAORIGEM = 1
+        GROUP BY B.CODPRODUTO
+      ) A LEFT JOIN (
+        SELECT productId, COUNT(*) qtd_beep
+        FROM PRODLOJAS_SERIES_MOVIMENTOS
+        WHERE outputModule = 'transfer'
+        GROUP BY productId
+      ) B ON A.CODPRODUTO = B.productId
+      WHERE (A.QUANTIDADE - ISNULL(B.qtd_beep, 0)) <> 0
+    ) D ON D.CODPRODUTO = A.CODIGO
+    WHERE ${where}`
+  },
+  stockNotBeep(condition, type = 'IN'){
+    const where = Object.keys(condition) === 'COD_ORIGINAL' 
+      ? `A.ALTERNATI IN (${Object.values(condition)})`
+      : `A.${Object.keys(condition)} ${type === 'IN' ? `IN (${Object.values(condition)})` : `LIKE '${Object.values(condition)}%'`}`
+
+    return `SELECT A.CODIGO, A.ALTERNATI COD_ORIGINAL, A.NOME, ISNULL(C.EST_LOJA, 0) EST_LOJA, ISNULL(B.qtdForecast, 0) qtdForecast, ISNULL(C.EST_LOJA, 0) - ISNULL(B.qtdForecast, 0) availableStock
+    FROM PRODUTOS A
+    LEFT JOIN (
+      SELECT B.COD_ORIGINAL, SUM(B.quantityForecast) qtdForecast
+      FROM SONOENTREGAS..FORECAST_SALES A
+      INNER JOIN SONOENTREGAS..FORECAST_PRODUCT B ON A.id = B.idForecastSale
+      INNER JOIN SONOENTREGAS..FORECAST C ON C.id = A.idForecast
+      WHERE (C.status IS NULL OR C.status = 1)
+      GROUP BY B.COD_ORIGINAL
+    ) B ON A.ALTERNATI = B.COD_ORIGINAL
+    LEFT JOIN PRODLOJAS C ON C.CODIGO = A.CODIGO
+    WHERE C.CODLOJA = 1
+    AND ${where}`
+  },
 }
